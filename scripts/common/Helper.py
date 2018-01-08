c9192578958f3fe12cadca01ffc0aacd295f0a05
huangyechuan <957760278@qq.com>
2018-01-08 19:17:47 +0800

    11

--- scripts/common/Helper.py
+++ scripts/common/Helper.py
@@ -1,5 +1,4 @@
 
-import socket
 import re
 
 def Round(num):
@@ -38,29 +37,6 @@ def convertDict(httpStr,str1,str2):
     print(result)
 
     return result
-
-
-
-def comp(x, y):
-    if x < y:
-        return 1
-    elif x > y:
-        return -1
-    else:
-        return 0
-
-test = []
-
-data1 = 2
-data2 = 1
-data3 = 0
-
-test.append(data1)
-test.append(data2)
-test.append(data3)
-
-print(2 not in test)
-
 
 
 
